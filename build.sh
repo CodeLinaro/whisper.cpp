e5ee247d906f2fdccbd9d73850e405f5a0daa78a
Brian Cain <bcain@quicinc.com>
2023-03-01 10:28:42 -0800

    add some hexagon build recipe stuff

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#export PATH=/prj/qct/llvm/release/internal/HEXAGON/branch-8.7lnx/latest/Tools/bin/:$PATH
+#export QEMU_LD_PREFIX=/prj/qct/llvm/release/internal/HEXAGON/branch-8.7lnx/latest/Tools/target/hexagon-linux-musl/
+
+#export PATH=/local/mnt/workspace/vp/checkout/qemu/build_dbg:$PATH
+#export PATH=/prj/qct/llvm/release/internal/HEXAGON/branch-8.7lnx/latest/Tools/bin/:$PATH
+#export QEMU_LD_PREFIX=/prj/qct/llvm/release/internal/HEXAGON/branch-8.7lnx/latest/Tools/target/hexagon-linux-musl/
+TOOLS=/pkg/qct/software/llvm/build_tools/clang+llvm-16.0.0-cross-hexagon-unknown-linux-musl/
+export PATH=${TOOLS}/x86_64-linux-gnu/bin/:$PATH
+export QEMU_LD_PREFIX=${TOOLS}/target/hexagon-unknown-linux-musl/
+
+cmake -GNinja \
+   -Bbuild_dbg \
+   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+   -DCMAKE_TOOLCHAIN_FILE=hexagon-unknown-linux-musl-clang.cmake \
+   -DCMAKE_INSTALL_PREFIX=~/scratch/tmp/whisper \
+   -S$PWD
+cmake --build ./build_dbg
+cmake --build ./build_dbg -- install
+
+echo qemu-hexagon  ./build_dbg/bin/bench -w 1 -t 1
+#qemu-hexagon -cpu any,paranoid-commit-state=on ./build_dbg/bin/bench -w 1 -t 1
---
+++ hexagon-clang.cmake
@@ -0,0 +1,5 @@
+
+set(CMAKE_C_COMPILER hexagon-linux-clang)
+set(CMAKE_CXX_COMPILER hexagon-linux-clang++)
+set(CMAKE_C_FLAGS "-mv65 -O2")
+set(CMAKE_CXX_FLAGS "-mv65 -O2")
