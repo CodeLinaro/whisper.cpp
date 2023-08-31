59353ffd203c792d76fb4601a5ab9c0344be3816
Brian Cain <bcain@quicinc.com>
2023-08-31 13:43:50 -0700

    add link flags

--- hexagon-unknown-linux-musl-clang.cmake
+++ hexagon-unknown-linux-musl-clang.cmake
@@ -3,6 +3,7 @@ set(CMAKE_C_COMPILER hexagon-unknown-linux-musl-clang)
 set(CMAKE_CXX_COMPILER hexagon-unknown-linux-musl-clang++)
 set(CMAKE_C_FLAGS "-mv67 -O3 -mhvx")
 set(CMAKE_CXX_FLAGS "-mv67 -O3 -mhvx")
+set(CMAKE_EXE_LINKER_FLAGS "-mv67 -O3 -mhvx")
 set(CMAKE_SYSTEM_PROCESSOR "hexagon")
 set(CMAKE_CROSSCOMPILING_EMULATOR qemu-hexagon)
 
