8069a2a27f2043750e630c74d5734c064f4809ba
Brian Cain <bcain@quicinc.com>
2023-09-10 21:39:36 -0700

    Use LSF to launch tests
    Using six host threads, the program completes with the accurate transcription
    in about 15 minutes on an LSF node:
    
        $ bsub -I -R "select[ubuntu20_llvm] && rusage[mem=12288]" qemu-hexagon ${INSTALL_DEST}/bin/main -t 6 -f samples/jfk.wav
        ...
        whisper_model_load: model ctx     =  140.66 MB
        whisper_model_load: model size    =  140.54 MB
        whisper_init_state: kv self size  =    5.25 MB
        whisper_init_state: kv cross size =   17.58 MB
    
        system_info: n_threads = 6 / 52 | AVX = 0 | AVX2 = 0 | AVX512 = 0 | FMA = 0 | NEON = 0 | ARM_FMA = 0 | F16C = 0 | FP16_VA = 0 | WASM_SIMD = 0 | BLAS = 0 | SSE3 = 0 | SSSE3 = 0 | VSX = 0 | COREML = 0 | OPENVINO = 0 |
    
        main: processing 'samples/jfk.wav' (176000 samples, 11.0 sec), 6 threads, 1 processors, lang = en, task = transcribe, timestamps = 1 ...
    
        [00:00:00.000 --> 00:00:11.000]   And so my fellow Americans, ask not what your country can do for you, ask what you can do for your country.
    
        whisper_print_timings:     load time =  1466.40 ms
        whisper_print_timings:     fallbacks =   0 p /   0 h
        whisper_print_timings:      mel time =  1145.36 ms
        whisper_print_timings:   sample time =  5321.89 ms /    27 runs (  197.11 ms per run)
        whisper_print_timings:   encode time = 858703.00 ms /     1 runs (858703.00 ms per run)
        whisper_print_timings:   decode time = 33177.91 ms /    27 runs ( 1228.81 ms per run)
        whisper_print_timings:    total time = 900065.69 ms
    
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- build.sh
+++ build.sh
@@ -6,18 +6,30 @@
 #export PATH=/local/mnt/workspace/vp/checkout/qemu/build_dbg:$PATH
 #export PATH=/prj/qct/llvm/release/internal/HEXAGON/branch-8.7lnx/latest/Tools/bin/:$PATH
 #export QEMU_LD_PREFIX=/prj/qct/llvm/release/internal/HEXAGON/branch-8.7lnx/latest/Tools/target/hexagon-linux-musl/
-TOOLS=/pkg/qct/software/llvm/build_tools/clang+llvm-16.0.0-cross-hexagon-unknown-linux-musl/
+#TOOLCHAIN_CMAKE=hexagon-linux-clang.cmake
+
+TOOLCHAIN_CMAKE=hexagon-unknown-linux-musl-clang.cmake
+#TOOLS=/pkg/qct/software/llvm/build_tools/clang+llvm-16.0.0-cross-hexagon-unknown-linux-musl/
+TOOLS=/local/mnt/workspace/install/clang+llvm-17.0.0-rc3-cross-hexagon-unknown-linux-musl/
+#TOOLS=/local/mnt/workspace/install/clang+llvm-17.0.0-rc3-cross-hexagon-unknown-linux-musl/
+TOOLS=/prj/qct/llvm/scratch/llvmops_users/bcain/tools/clang+llvm-17.0.0-rc3-cross-hexagon-unknown-linux-musl
 export PATH=${TOOLS}/x86_64-linux-gnu/bin/:$PATH
 export QEMU_LD_PREFIX=${TOOLS}/x86_64-linux-gnu/target/hexagon-unknown-linux-musl
 
+INSTALL_DEST=/prj/qct/llvm/scratch/llvmops_users/bcain/tmp/whisper
+
 cmake -GNinja \
    -Bbuild_dbg \
+   -DWHISPER_BUILD_TESTS=ON \
    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-   -DCMAKE_TOOLCHAIN_FILE=hexagon-unknown-linux-musl-clang.cmake \
-   -DCMAKE_INSTALL_PREFIX=~/scratch/tmp/whisper \
+   -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_CMAKE} \
+   -DCMAKE_INSTALL_PREFIX=${INSTALL_DEST} \
    -S$PWD
 cmake --build ./build_dbg
 cmake --build ./build_dbg -- install
 
-echo qemu-hexagon  ./build_dbg/bin/bench -w 1 -t 1
+#qemu-hexagon  ./build_dbg/bin/bench -w 1 -t 1
+cp -ra ./models ./samples ${INSTALL_DEST}
+cd ${INSTALL_DEST}
+bsub -I -R "select[ubuntu20_llvm] && rusage[mem=12288]" qemu-hexagon ${INSTALL_DEST}/bin/main -t 6 -f samples/jfk.wav
 #qemu-hexagon -cpu any,paranoid-commit-state=on ./build_dbg/bin/bench -w 1 -t 1
